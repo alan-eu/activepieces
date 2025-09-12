3a01be4940d2fe9fc4fec26cc02f60ae82f71b61
meenulekha-premakumar <meenu-lekha.premakumar@alan.eu>
2025-09-12 17:36:40 +0200

    feat: add message contains keyword trigger

--- packages/pieces/community/slack/src/index.ts
+++ packages/pieces/community/slack/src/index.ts
@@ -46,6 +46,7 @@ import { findUserByIdAction } from './lib/actions/find-user-by-id';
 import { newUserTrigger } from './lib/triggers/new-user';
 import { newSavedMessageTrigger } from './lib/triggers/new-saved-message';
 import { newTeamCustomEmojiTrigger } from './lib/triggers/new-team-custom-emoji';
+import { messageContainsKeywordTrigger } from './lib/triggers/message-contains-keyword';
 import { inviteUserToChannelAction } from './lib/actions/invite-user-to-channel';
 import { listUsers } from './lib/actions/list-users';
 import { deleteMessageAction } from './lib/actions/delete-message';
@@ -236,6 +237,7 @@ export const slack = createPiece({
     newUserTrigger,
     newSavedMessageTrigger,
     newTeamCustomEmojiTrigger,
+    messageContainsKeywordTrigger,
   ],
 });
 
